703acaa717e14f4d0c4517e1816586508a854358
Thomas peters <shadowatubuntu@gmail.com>
2017-04-19 09:58:42 -0600

    Added getch() movment with proper game exits

--- main.cpp
+++ main.cpp
@@ -1,5 +1,64 @@
+//#include <getch.h>
+#include <stdio.h>
 #include <iostream>
 #include <cstdlib>
+//#include <conio.h> //windows
+#include <curses.h>
+
+
+#include <termios.h>
+
+static struct termios old, newer;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  newer = old; /* make new settings same as old settings */
+  newer.c_lflag &= ~ICANON; /* disable buffered i/o */
+  newer.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &newer); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+//char getch(void)
+//{
+//  return getch_(0);
+//}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+  return getch_(1);
+}
+
+/* Let's test it out */
+//int main(void) {
+//  char c;
+//  printf("(getche example) please type a letter: ");
+//  c = getche();
+//  printf("\nYou typed: %c\n", c);
+//  printf("(getch example) please type a letter...");
+//  c = getch();
+//  printf("\nYou typed: %c\n", c);
+//  return 0;
+//}
 
 using namespace std;
 bool gameOver;
@@ -35,6 +94,11 @@ void Draw()
 		{
 			if (j == 0)
 				cout << "#";
+			if (i == y && j == x)
+				cout << "0";
+			else if (i == fruitY && j == fruitX)
+				cout << "F";
+			else
 				cout << " ";
 
 			if (j == width - 1)
@@ -50,12 +114,51 @@ void Draw()
 
 void Input()
 {
+	char c;
+	c = getche();
+	//if (_kbhit())
+	//{
+		switch (c)
+		{
+			case 'a':
+				dir = LEFT;
+				break;
+			case 'd':
+				dir = RIGHT;
+				break;
+			case 's':
+				dir = DOWN;
+				break;
+			case 'w':
+				dir = UP;
+				break;
+			case 'x':
+				gameOver = true;
+				break;
+		}
+	//}
 
 }
 
 void Logic()
 {
-
+	switch (dir)
+	{
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	}
+	if (x > width || x < 0 || y > height || y < 0)
+		gameOver = true;
 }
 
 
