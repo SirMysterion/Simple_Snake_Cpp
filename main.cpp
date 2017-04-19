6b38db45221e24061b5a10d7f2787ae17aa059e7
Thomas peters <shadowatubuntu@gmail.com>
2017-04-19 07:50:01 -0600

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+
+
+
+void Setup()
+{
+	gameOver = false;
+}
+
+void Draw()
+{
+
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+
+int main()
+{
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		//Sleep(10); sleep(10);
+	}
+	return 0;
+}
