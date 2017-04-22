7dcd3518a4f3eaa644b458ce7f4a12301d3e539b
Thomas peters <shadowatubuntu@gmail.com>
2017-04-21 23:46:42 -0600

    Tutorial Finished snake game

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
- //#include <getch.h>
 #include <stdio.h>
 #include <iostream>
 #include <cstdlib>
 //#include <conio.h> //windows
 #include <curses.h>
-
-
 #include <termios.h>
 
 static struct termios old, newer;
@@ -65,6 +62,9 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
@@ -99,7 +99,21 @@ void Draw()
 			else if (i == fruitY && j == fruitX)
 				cout << "F";
 			else
-				cout << " ";
+			{
+				bool print = false;
+				for (int k = 0; k < nTail; k++)
+				{
+					if (tailX[k] == j && tailY[k] == i)
+					{
+						cout << "o";
+						print = true;
+					}
+
+				}
+				if (!print)
+					cout << " ";
+			}
+//				cout << " ";
 
 			if (j == width - 1)
 				cout << "#";
@@ -143,6 +157,21 @@ void Input()
 
 void Logic()
 {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2x, prev2y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++)
+	{
+		prev2x = tailX[i];
+		prev2y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2x;
+		prevY = prev2y;
+	}
+
 	switch (dir)
 	{
 	case LEFT:
@@ -160,11 +189,15 @@ void Logic()
 	}
 	if (x > width-1 || x < 0 || y > height-1 || y < 0)
 		gameOver = true;
+	for (int i = 0; i < nTail; i++)
+		if (tailX[i] == x && tailY[i] == y)
+			gameOver = true;
 	if (x == fruitX && y == fruitY)
 	{
 		score +=10;
 		fruitX = rand() % width;
 	        fruitY = rand() % height;
+		nTail++;
 	}
 }
 
@@ -177,7 +210,7 @@ int main()
 		Draw();
 		Input();
 		Logic();
-		//Sleep(10); sleep(10);
+		//sleep(10); //Sleep(10);
 	}
 	return 0;
 }
